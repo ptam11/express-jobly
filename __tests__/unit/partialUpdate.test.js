e40113426edbb903ddd67f6b26810688da7b63cd
Ricky Bartenslager <RickyBartenslager@gmail.com>
2019-05-08 10:51:18 -0700

    still wip

--- __tests__/unit/partialUpdate.test.js
+++ __tests__/unit/partialUpdate.test.js
@@ -10,14 +10,14 @@ beforeEach(createData)
 describe("sqlForPartialUpdate()", () => {
   test("should generate a proper partial update query with just 1 field",
       async function () {
-    const injection = sqlForPartialUpdate(`companies`, {"junk": "0"}, `num_employees; INSERT INTO companies (
+    const injection = sqlForPartialUpdate(`companies`, {"junk": "0"}, `num_employees = 1; INSERT INTO companies (
       handle,
       name,
       num_employees,
       description,
       logo_url)
-  VALUES (hahaahaa, IhackedU, 1, hacked)
-  RETURNING handle;`, 1)
+    VALUES (hahaahaa, IhackedU, 1, hacked)
+    RETURNING handle;`, 1)
     let worked = await db.query(injection.query)
     expect(worked).toEqual('hahaahaa')
     const companies = `SELECT handle FROM companies`
