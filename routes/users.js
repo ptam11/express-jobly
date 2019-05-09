0ccd09e1e564c60937fdc5486a57d740bff42577
Parco Tam <ptam1556@gmail.com>
2019-05-09 15:43:58 -0700

    WIP completed users routes but need to test

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = new express.Router();
-const Job = require('../models/jobModel');
+const User = require('../models/userModel');
 const jsonschema = require('jsonschema');
-const jobSchema = require('../schema/jobSchema');
-const expressError = require('../helpers/expressError');
+const userSchema = require('../schema/userSchema');
+const ExpressError = require('../helpers/ExpressError');
 
 // const jwt = require("jsonwebtoken");
 const { SECRET_KEY } = require('../config');
@@ -12,8 +12,8 @@ const { SECRET_KEY } = require('../config');
 router.get('/' , async function(req, res, next){
   let params = req.query;
   try{
-    let result = await Job.findAll(params);
-    return res.status(200).json({jobs: result});
+    let result = await User.findAll(params);
+    return res.status(200).json({users: result});
   } catch (err){
     next(err);
   }
@@ -21,63 +21,65 @@ router.get('/' , async function(req, res, next){
 
 router.post('/', async function(req, res, next){
   try {
-    const isValid = jsonschema.validate(req.body, jobSchema);
+    const isValid = jsonschema.validate(req.body, userSchema);
     
     if(isValid.errors.length) {
-      throw new expressError('invalid form', 400);
+      throw new ExpressError('invalid form', 400);
     }
     try{
-      const results = await Job.create(req.body);
-      return res.status(201).json({job: results});
+      const results = await User.create(req.body);
+      return res.status(201).json({user: results});
     } catch (err){
-      throw new expressError('ERROR: no handle of that name', 400);
+      throw new ExpressError('ERROR: no handle of that name', 400);
     }
   } catch(err) {
     next(err);
   }
 });
 
-router.get('/:id', async function(req, res, next){
+router.get('/:username', async function(req, res, next){
   try {
-    const results = await Job.findOne(req.params.id);
+    const results = await User.findOne(req.params.username);
     
-    if (!results.rowCount) {
-      throw new expressError('job not found', 404);
+    if (!results) {
+      throw new ExpressError('user not found', 404);
     }
-    return res.json({job: results.rows[0]});
+    return res.json({user: results});
   } catch(err) {
     next(err);
   }
 });
 
-router.patch('/:id', async function(req, res, next){
+router.patch('/:username', async function(req, res, next){
   try {
-    const existingData = await Job.findOne(req.params.id);
-    if (!existingData.rowCount) {
-      throw new expressError('job not found', 404);
+    const existingData = await User.findOne(req.params.username);
+    if (!existingData) {
+      throw new ExpressError('user not found', 404);
     }
     // combining the partial data to pass schema
     const combinedData = Object.assign(existingData, req.body);
-    const isValid = jsonschema.validate(combinedData, jobSchema);
+    const isValid = jsonschema.validate(combinedData, userSchema);
     if(isValid.errors.rowCount) {
-      throw new expressError('invalid update job form', 400);
+      // get all errors from schema for all invalid fields
+      let listOfErrors = isValid.errors.map(error => error.stack);
+      throw new ExpressError(listOfErrors, 400);
     }
     // using req.body to patch only requested data
-    const results = await Job.update(req.params.id, req.body);
-    return res.json({job: results.rows[0]});
+    const results = await User.update(req.params.username, req.body);
+    return res.json({user: results});
   } catch(err) {
     next(err);
   }
 });
 
-router.delete('/:id', async function(req, res, next){
+router.delete('/:username', async function(req, res, next){
   try {
-    const results = await Job.delete(req.params.id);
+    const results = await User.delete(req.params.username);
     
     if(results.rowCount === 1) {
-      return res.json({message: "Job deleted"});
+      return res.json({message: "user deleted"});
     } else {
-      throw new expressError("job not found", 404);
+      throw new ExpressError("user not found", 404);
     }
   } catch(err) {
     next(err);
