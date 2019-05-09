43798e3ebbdfe007b0f22349a8fb0e6e1678bc42
Jason Matthias <jpmatthias90@gmail.com>
2019-05-09 11:29:56 -0700

    modified helper function to protect from changing job ID on patch requests to /jobs

--- helpers/partialUpdate.js
+++ helpers/partialUpdate.js
@@ -13,15 +13,12 @@
  */
 
 function sqlForPartialUpdate(table, items, targetKey, id) {
-  // keep track of item indexes
-  // store all the columns we want to update and associate with vals
-
   let idx = 1;
   let columns = [];
 
   // filter out keys that start with "_" -- we don't want these in DB
   for (let key in items) {
-    if (key.startsWith("_")) {
+    if (key.startsWith("_") || key === 'id') {
       delete items[key];
     }
   }
