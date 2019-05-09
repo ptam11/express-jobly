2e02cc538d4a17527ca1d5c7c25c6f7cc1a116c2
Ricky Bartenslager <RickyBartenslager@gmail.com>
2019-05-09 10:35:48 -0700

    added jobs test and modified test-common

--- _test-common.js
+++ _test-common.js
@@ -19,9 +19,11 @@ async function createData() {
     "seattle based coffee company, we burn our coffee often, includes sugar",
     "https://starbucks.com/logo.jpg",
   ]);
-
-}
-
+  await db.query (`INSERT INTO jobs
+      (title, salary, equity, company_handle, date_posted)
+      Values ('tester', 1000.00, 0, 'SBUX', CURRENT_TIMESTAMP);`
+    )
+  }
 module.exports = {
   createData
 };
